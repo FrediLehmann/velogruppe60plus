5c90bbc3ca5702db9730c39efce3978d44631332
FrediLehmann <frederic.lehmann@bluewin.ch>
2022-11-27 16:56:11 +0100

    remove unused component

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -1,6 +1,5 @@
 import {
   Button,
-  Center,
   Container,
   FormControl,
   FormErrorMessage,
@@ -17,75 +16,73 @@ const Login = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   return (
     <Container maxW="lg">
-      <Center>
-        <Stack
-          spacing="6"
-          py={["0", "8"]}
-          px={["4", "10"]}
-          bg={["transparent", "white"]}
-          boxShadow={["none", "md"]}
-          borderRadius={["none", "xl"]}
+      <Stack
+        spacing="6"
+        py={["0", "8"]}
+        px={["4", "10"]}
+        bg={["transparent", "white"]}
+        boxShadow={["none", "md"]}
+        borderRadius={["none", "xl"]}
+      >
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={object({
+            email: string()
+              .email("Invalide Email Addresse.")
+              .required("Email Addresse wird benötigt."),
+            password: string()
+              .min(6, "Passwort ist zu kurz.")
+              .required("Passwort wird benötigt."),
+          })}
+          onSubmit={async (values) => {
+            setIsSubmitting(true);
+            console.log(values);
+          }}
         >
-          <Formik
-            initialValues={{ email: "", password: "" }}
-            validationSchema={object({
-              email: string()
-                .email("Invalide Email Addresse.")
-                .required("Email Addresse wird benötigt."),
-              password: string()
-                .min(6, "Passwort ist zu kurz.")
-                .required("Passwort wird benötigt."),
-            })}
-            onSubmit={async (values) => {
-              setIsSubmitting(true);
-              console.log(values);
-            }}
-          >
-            {(props) => (
-              <Form id="login">
-                <Stack spacing="5">
-                  <Field name="email">
-                    {({ field, form }: FieldProps) => (
-                      <FormControl
-                        isRequired
-                        isInvalid={
-                          (form.errors.email && form.touched.email) as boolean
-                        }
-                      >
-                        <FormLabel>Email Addresse</FormLabel>
-                        <Input {...field} background="white" />
-                        <FormErrorMessage>
-                          {form.errors?.email as string}
-                        </FormErrorMessage>
-                      </FormControl>
-                    )}
-                  </Field>
-                  <Field name="password">
-                    {({ field, form }: FieldProps) => (
-                      <PasswordField
-                        isInvalid={
-                          (form.errors.password &&
-                            form.touched.password) as boolean
-                        }
-                        errorMessage={form.errors?.password as string}
-                        field={field}
-                      />
-                    )}
-                  </Field>
-                </Stack>
-              </Form>
-            )}
-          </Formik>
-          <Button
-            type="submit"
-            form="login"
-            colorScheme="blue"
-            isLoading={isSubmitting}
-          >
-            Einloggen
-          </Button>
-        </Stack>
-      </Center>
+          {(props) => (
+            <Form id="login">
+              <Stack spacing="5">
+                <Field name="email">
+                  {({ field, form }: FieldProps) => (
+                    <FormControl
+                      isRequired
+                      isInvalid={
+                        (form.errors.email && form.touched.email) as boolean
+                      }
+                    >
+                      <FormLabel>Email Addresse</FormLabel>
+                      <Input {...field} background="white" />
+                      <FormErrorMessage>
+                        {form.errors?.email as string}
+                      </FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field name="password">
+                  {({ field, form }: FieldProps) => (
+                    <PasswordField
+                      isInvalid={
+                        (form.errors.password &&
+                          form.touched.password) as boolean
+                      }
+                      errorMessage={form.errors?.password as string}
+                      field={field}
+                    />
+                  )}
+                </Field>
+              </Stack>
+            </Form>
+          )}
+        </Formik>
+        <Button
+          type="submit"
+          form="login"
+          colorScheme="blue"
+          isLoading={isSubmitting}
+        >
+          Einloggen
+        </Button>
+      </Stack>
     </Container>
   );
 };
