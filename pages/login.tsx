1b355fee2cf4fc0493e93a4c5cb99181b2efb438
FrediLehmann <frederic.lehmann@bluewin.ch>
2022-11-27 17:28:59 +0100

    add signed in guard to login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,27 @@
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { Login as LoginComponent, PageFrame } from "components";
+import { GetServerSidePropsContext } from "next";
 import Head from "next/head";
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const supabase = createServerSupabaseClient(ctx);
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session)
+    return {
+      redirect: {
+        destination: "/admin",
+        permanent: false,
+      },
+    };
+
+  return {
+    props: {},
+  };
+};
 
 const Login = () => {
   return (
