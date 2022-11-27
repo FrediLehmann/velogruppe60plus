fc51e395b29f835c8098b06d351435bcd49f7abe
FrediLehmann <frederic.lehmann@bluewin.ch>
2022-11-27 17:23:15 +0100

    add sign out

--- components/PageFrame/components/Header.tsx
+++ components/PageFrame/components/Header.tsx
@@ -1,7 +1,13 @@
 import { Button, ButtonGroup, Container, Link, Text } from "@chakra-ui/react";
+import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 import NextLink from "next/link";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const user = useUser();
+  const router = useRouter();
+  const supabaseClient = useSupabaseClient();
+
   return (
     <Container
       as="header"
@@ -13,7 +19,19 @@ const Header = () => {
       <Text fontSize={["xl", "2xl"]} fontWeight="bold">
         Velogruppe 60+
       </Text>
-      <ButtonGroup>
+      <ButtonGroup size="sm" gap="4">
+        {user && (
+          <Button
+            as={Link}
+            variant="link"
+            onClick={async () => {
+              await supabaseClient.auth.signOut();
+              router.push("/");
+            }}
+          >
+            Abmelden
+          </Button>
+        )}
         <NextLink href="/alle-touren" passHref legacyBehavior>
           <Button as={Link} variant="link">
             Alle Touren
