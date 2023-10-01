fac0eb623645a8ddb7e7078a82b47fb881df8f1c
FrediLehmann <frederic.lehmann@bluewin.ch>
2023-10-01 18:11:18 +0200

    Add tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="de">
       <Head />
       <body>
+        <Script
+          src="/tellytics.min.js"
+          data-tellytics-tracking-id="ef563f39-aa47-4d8c-b53f-c3265a49f90d"
+          type="module"
+          strategy="beforeInteractive"
+        />
         <Main />
         <NextScript />
       </body>
