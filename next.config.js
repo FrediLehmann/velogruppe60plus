b5f33f3cb3d488fa028cc0f70e5d8e7891d83668
FrediLehmann <frederic.lehmann@bluewin.ch>
2024-03-18 22:28:30 +0100

    remove obsolete config

--- next.config.js
+++ next.config.js
@@ -9,7 +9,6 @@ const nextConfig = {
     REGENERATE_TOKEN: regenerateToken
   },
   images: {
-    domains: ['localhost'],
     remotePatterns: [
       {
         protocol: imageDomain.split('://')[0],
