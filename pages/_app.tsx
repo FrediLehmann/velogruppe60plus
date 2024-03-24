a284d3256f3ad35587747d6f7218112a80ef6151
FrediLehmann <frederic.lehmann@bluewin.ch>
2024-03-24 09:04:45 +0100

    move script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
 import { Database } from 'types/Database.types';
 import theme from 'lib/theme';
@@ -14,6 +15,11 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Script
+        src="https://tellytics.vercel.app/public/tellytics.min.js"
+        type="module"
+        data-tellytics="e344db29-d13a-41cf-ab25-5d75fca3b810"
+      />
       <SessionContextProvider
         supabaseClient={supabaseClient}
         initialSession={pageProps.initialSession}>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="de">
       <Head />
       <body>
-        <Script
-          src="https://tellytics.vercel.app/public/tellytics.min.js"
-          type="module"
-          data-tellytics="e344db29-d13a-41cf-ab25-5d75fca3b810"
-        />
         <Main />
         <NextScript />
       </body>
