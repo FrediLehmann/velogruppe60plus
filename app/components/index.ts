99765cdcbe3866afd374be95aef5ae0301971384
FrediLehmann <frederic.lehmann@bluewin.ch>
2024-05-10 10:19:45 +0200

    Use new page

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,3 +1,4 @@
+export { default as CurrentTour } from './CurrentTour';
 export { default as PageFrame } from './PageFrame';
 export { default as Providers } from './Providers';
 export { default as TourView } from './TourView';
--- app/some/components/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as CurrentTour } from './CurrentTour';
