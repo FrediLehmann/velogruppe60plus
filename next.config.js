95502b8b54a358ed28cf43b6cbef90cd1f5925e3
FrediLehmann <frederic.lehmann@bluewin.ch>
2024-11-09 10:00:10 +0100

    upgrade packages and add prettier sort

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
-const regenerateToken = process.env.NEXT_REGENERATE_TOKEN;
 const imageDomain = process.env.NEXT_PUBLIC_SUPABASE_URL;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  env: {
-    REGENERATE_TOKEN: regenerateToken
-  },
   images: {
     remotePatterns: [
       {
