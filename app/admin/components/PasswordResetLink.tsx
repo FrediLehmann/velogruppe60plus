4560194ab0efa226b695d0dc4fd83febc62b6f4a
FrediLehmann <frederic.lehmann@bluewin.ch>
2024-11-10 10:44:52 +0100

    add margin below

--- app/admin/components/PasswordResetLink.tsx
+++ app/admin/components/PasswordResetLink.tsx
@@ -2,7 +2,7 @@ import { Link } from '@chakra-ui/react';
 
 export default function PasswordResetLink() {
 	return (
-		<Link href="/change-password" color="green.700">
+		<Link href="/change-password" color="green.700" mb="12">
 			Passwort ändern
 		</Link>
 	);
