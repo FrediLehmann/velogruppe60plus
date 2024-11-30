66819a7fbc3e178cddf43c544cb773ac9b1d6b18
FrediLehmann <frederic.lehmann@bluewin.ch>
2024-11-30 14:46:05 +0100

    change accident info page

--- app/unfallverhalten/page.tsx
+++ app/unfallverhalten/page.tsx
@@ -1,4 +1,4 @@
-import { Heading, ListItem, OrderedList, Text } from '@chakra-ui/react';
+import { Heading, ListItem, OrderedList, Text, UnorderedList } from '@chakra-ui/react';
 
 export const metadata = {
 	title: 'Velogruppe 60+ Sensetal | Unfallverhalten',
@@ -13,141 +13,155 @@ export default function Unfallverhlaten() {
 				Unfallverhalten
 			</Heading>
 			<Text>
-				Im Falle eines Unfalls mit einem eBike gibt es einige wichtige Schritte, die Sie beachten
-				sollten:
+				Bei einem Unfall mit einem eBike ist ein systematisches und besonnenes Vorgehen
+				entscheidend, um die Situation sicher zu bewältigen. Im Folgenden wird eine strukturierte
+				Anleitung für das Verhalten im Falle eines Unfalls dargestellt.
 			</Text>
-			<OrderedList my="4" px="6">
+			<OrderedList fontWeight="semibold" my="4" px="6">
 				<ListItem>
-					Beruhigen Sie sich und bleiben Sie ruhig: Versuchen Sie, ruhig zu bleiben und sich zu
-					konzentrieren. Atmen Sie tief durch und versuchen Sie, Ihre Gedanken zu sammeln.
+					Ruhe bewahren und Überblick verschaffen
+					<Text as="span" fontWeight="normal">
+						Das eigene Verhalten hat massgeblichen Einfluss auf den weiteren Verlauf der
+						Unfallbewältigung. Ruhe zu bewahren und sich zu konzentrieren, ist daher essenziell.
+						Tiefe Atemzüge helfen dabei, Gedanken zu ordnen und die Situation klar zu erfassen.
+					</Text>
 				</ListItem>
 				<ListItem>
-					Rufen Sie umgehend den Notruf: Wenn Sie oder jemand anderes verletzt wurde, rufen Sie so
-					schnell wie möglich den Notruf (in Schweiz: 144). Geben Sie an, wo Sie sind und
-					beschreiben Sie die Art der Verletzungen.
+					Notruf verständigen
+					<Text as="span" fontWeight="normal">
+						Im Falle von Verletzungen ist der umgehende Notruf (in der Schweiz: 144) zu
+						kontaktieren. Dabei sind der Unfallort, die Art des Vorfalls sowie die Schwere der
+						Verletzungen präzise zu beschreiben.
+					</Text>
 				</ListItem>
 				<ListItem>
-					Versuchen Sie, die Unfallstelle zu sichern: Wenn möglich, sichern Sie die Unfallstelle, um
-					weitere Unfälle zu vermeiden. Stellen Sie das eBike an einem sicheren Ort ab und setzen
-					Sie warneinblendende Triangel auf, wenn vorhanden.
+					Absicherung der Unfallstelle
+					<Text as="span" fontWeight="normal">
+						Die Unfallstelle ist, sofern möglich, abzusichern, um Folgeunfälle zu vermeiden. Dies
+						kann durch das Abstellen des eBikes an einem sicheren Ort oder das Platzieren von
+						Warndreiecken erfolgen.
+					</Text>
 				</ListItem>
 				<ListItem>
-					Helfen Sie Verletzten: Wenn jemand verletzt wurde, versuchen Sie, ihm oder ihr zu helfen.
-					Beruhigen Sie die Person und versuchen Sie, die Verletzungen zu stabilisieren, indem Sie
-					etwa blutende Wunden abdecken oder Schmerzen lindern.
+					Erste Hilfe leisten
+					<Text as="span" fontWeight="normal">
+						Die Versorgung von Verletzten hat oberste Priorität. Blutungen sollten, soweit möglich,
+						gestoppt, Verletzungen stabilisiert und betroffene Personen beruhigt werden.
+						Gleichzeitig ist darauf zu achten, die eigene Sicherheit nicht zu gefährden.
+					</Text>
 				</ListItem>
 				<ListItem>
-					Nehmen Sie Hilfe an: Wenn andere Personen Ihnen helfen möchten, nehmen Sie diese Hilfe
-					dankbar an. Sie können von anderen Personen etwa bei der Sicherung der Unfallstelle, der
-					Versorgung von Verletzungen oder dem Rufen des Notrufs unterstützt werden.
+					Unterstützung annehmen
+					<Text as="span" fontWeight="normal">
+						Hilfsangebote von anwesenden Personen sollten dankbar angenommen werden. Diese
+						Unterstützung kann bei der Absicherung, der Ersthilfe oder der Alarmierung des
+						Rettungsdienstes von entscheidender Bedeutung sein.
+					</Text>
 				</ListItem>
 				<ListItem>
-					Verfolgen Sie die Anweisungen der Helfer: Folgen Sie den Anweisungen der Helfer, wenn
-					diese Ihnen sagen, was Sie tun sollen. Sie wissen möglicherweise, wie man in einer
-					bestimmten Situation am besten vorgeht und können Ihnen dabei helfen, sich sicherer zu
-					fühlen.
+					Anleitung durch Fachkräfte folgen
+					<Text as="span" fontWeight="normal">
+						Nach Eintreffen der Rettungskräfte ist es wichtig, deren Anweisungen zu folgen. Diese
+						sind speziell geschult und in der Lage, die Situation optimal zu beurteilen und
+						entsprechende Massnahmen einzuleiten.
+					</Text>
 				</ListItem>
 				<ListItem>
-					Geben Sie Auskunft über den Unfall: Geben Sie den Helfern so viele Informationen wie
-					möglich über den Unfall, damit diese wissen, wie sie Ihnen am besten helfen können.
-					Erzählen Sie ihnen etwa, wo der Unfall passiert ist, wie es dazu kam und ob jemand
-					verletzt wurde.
+					Dokumentation des Unfalls
+					<Text as="span" fontWeight="normal">
+						Zur späteren Klärung ist eine umfassende Dokumentation ratsam:
+						<UnorderedList>
+							<ListItem>Fotos der Unfallstelle, des eBikes und etwaiger Schäden.</ListItem>
+							<ListItem>
+								Notizen zu relevanten Details wie Unfallzeit, Wetterbedingungen und den genauen
+								Hergang.
+							</ListItem>
+						</UnorderedList>
+					</Text>
 				</ListItem>
 				<ListItem>
-					Machen Sie Fotos: Machen Sie Fotos von der Unfallstelle und dem eBike, um Beweismaterial
-					zu sammeln. Diese Fotos können später bei der Schadensregulierung hilfreich sein.
-				</ListItem>
-				<ListItem>
-					Informieren Sie die Polizei: Melden Sie den Unfall der Polizei (in der Schweiz 117)v, wenn
-					es sich um einen schweren Unfall handelt oder wenn es Sachschäden oder Verletzungen gibt.
-					Die Polizei wird eine Unfallprotokoll erstellen, das für die Schadensregulierung wichtig
-					sein kann.
-				</ListItem>
-				<ListItem>
-					Machen Sie sich Notizen: Machen Sie sich Notizen zu dem Unfall, um Details später besser
-					zu erinnern. Notieren Sie sich Dinge wie den Unfallort, die Uhrzeit, das Wetter und die
-					Umstände des Unfalls.
-				</ListItem>
-				<ListItem>
-					Kontaktieren Sie Ihre Versicherung: Informieren Sie Ihre Versicherung über den Unfall, um
-					die Schadensregulierung in die Wege zu leiten. Stellen Sie sicher, dass Sie alle
-					notwendigen Unterlagen, wie das Unfallprotokoll der Polizei, bereithalten.
+					Meldung an Polizei und Versicherung
+					<Text as="span" fontWeight="normal">
+						Bei schweren Unfällen, Personenschäden oder erheblichen Sachschäden ist die Polizei (in
+						der Schweiz: 117) zu verständigen, um ein Unfallprotokoll zu erstellen. Dieses Dokument
+						ist später für die Schadensregulierung erforderlich. Ebenso ist die Versicherung zeitnah
+						zu informieren, wobei alle relevanten Unterlagen bereitzuhalten sind.
+					</Text>
 				</ListItem>
 			</OrderedList>
-			<Heading as="h3" size="md" mt="8" mb="4">
-				App EchoSOS
-			</Heading>
 			<Text>
-				EchoSOS ist eine App, die im Notfall als zusätzliche Sicherheit dienen kann. Die App kann
-				auf dem Smartphone installiert werden und ermöglicht es dem Nutzer, sich in akuten Notfällen
-				direkt mit dem Notruf in Verbindung zu setzen. Die App kann auch automatisch den Standort
-				des Nutzers an den Notruf senden, um die Einsatzkräfte schneller zu Ihnen zu führen. EchoSOS
-				ist vor allem für Menschen nützlich, die sich in abgelegenen Gebieten oder alleine unterwegs
-				sind und schnell Hilfe benötigen. Es kann auch für Menschen hilfreich sein, die aufgrund von
-				Vorerkrankungen oder Behinderungen möglicherweise nicht in der Lage sind, selbst den Notruf
-				zu wählen. Es ist wichtig zu beachten, dass EchoSOS kein Ersatz für den Notruf ist und immer
-				der Notruf (in Schweiz: 144) als erste Wahl für Notfälle angesehen werden sollte. Die App
-				sollte als zusätzliche Sicherheit betrachtet werden und nicht als primäre Möglichkeit zur
-				Hilfeleistung
+				Eine klare Struktur und umsichtiges Handeln im Falle eines eBike-Unfalls sind essenziell, um
+				Verletzungen zu minimieren und die Klärung des Vorfalls zu unterstützen. Die oben genannten
+				Schritte bieten einen praktischen Leitfaden, der sowohl Sicherheit als auch Effizienz
+				gewährleistet.
 			</Text>
 			<Heading as="h3" size="md" mt="8" mb="4">
-				Nach Unfall E-Bike Tour fortsetzen
+				Fortsetzung einer eBike-Tour nach einem Unfall
 			</Heading>
 			<Text>
-				Es ist wichtig, dass alle Beteiligten bei einem Unfall auf einem eBike sicherstellen, dass
-				sie sich in einer sicheren Umgebung befinden und dass niemand verletzt ist. Wenn jemand
-				verletzt ist, sollte sofort medizinische Hilfe geholt werden. Wenn alle Beteiligten
-				unverletzt sind, sollten sie sich um das beschädigte Fahrrad kümmern und entscheiden, ob es
-				repariert werden kann oder ob es abtransportiert werden muss. Wenn möglich, sollte das
-				Fahrrad sicher gestellt werden, damit es nicht gestohlen wird. Wenn der Unfall von einem
-				anderen Fahrzeug verursacht wurde, sollten alle Beteiligten die Polizei informieren und eine
-				Unfallmeldung ausfüllen. Es ist auch wichtig, dass alle Beteiligten ihre Kontaktdaten und
-				Versicherungsinformationen austauschen. Wenn alle Beteiligten sich in einer sicheren
-				Umgebung befinden und das Fahrrad repariert oder abtransportiert wurde, können die anderen
-				Teilnehmer der Tour entscheiden, ob sie fortfahren oder die Tour abbrechen möchten. Wenn sie
-				beschließen, die Tour fortzusetzen, sollten sie sicherstellen, dass sie alle notwendigen
-				Vorsichtsmaßnahmen ergreifen, um sich selbst und andere zu schützen. Dazu gehört, dass sie
-				sich an die Verkehrsregeln halten und sich auf die Straße konzentrieren. Es ist auch
-				wichtig, dass alle Teilnehmer aufeinander achten und sich gegenseitig unterstützen, wenn es
-				nötig ist. Es ist auch ratsam, dass die Teilnehmer der Tour ihre Kommunikation und
-				Koordination verbessern, um sicherzustellen, dass sie alle über die aktuelle Situation und
-				den weiteren Verlauf der Tour informiert sind. Insgesamt sollten alle Teilnehmer der Tour
-				verantwortungsbewusst und sicher fahren und sich gegenseitig unterstützen, um
-				sicherzustellen, dass die Tour erfolgreich fortgesetzt werden kann.
+				Nach einem Unfall während einer eBike-Tour sollten alle Beteiligten zunächst sicherstellen,
+				dass sie sich an einem sicheren Ort befinden und keine Verletzungen vorliegen. Bei
+				Verletzungen ist sofort medizinische Hilfe zu rufen. Wenn niemand verletzt ist, sollte das
+				beschädigte eBike gesichert oder transportiert werden. Bei einem Unfall mit einem anderen
+				Fahrzeug ist die Polizei zu verständigen und eine Unfallmeldung auszufüllen. Alle
+				Beteiligten sollten ihre Kontaktdaten und Versicherungsinformationen austauschen.
+			</Text>
+			<Text mt="3">
+				Entscheiden sich die Teilnehmer, die Tour fortzusetzen, müssen sie auf Sicherheit achten,
+				die Verkehrsregeln befolgen und aufeinander achten. Eine gute Kommunikation und Koordination
+				sind wichtig, um die Tour verantwortungsbewusst und sicher fortzusetzen.
+			</Text>
+			<Heading as="h3" size="md" mt="8" mb="4">
+				EchoSOS: Notfall-App zur Sicherheitsunterstützung
+			</Heading>
+			<Text>
+				EchoSOS ist eine mobile Anwendung, die es Nutzern ermöglicht, im Notfall direkt den Notruf
+				zu kontaktieren und automatisch den Standort zu übermitteln, um Einsatzkräfte schneller zu
+				führen. Sie ist besonders nützlich für Menschen in abgelegenen Gebieten oder mit
+				gesundheitlichen Einschränkungen, die Schwierigkeiten haben könnten, selbst den Notruf zu
+				wählen. Dennoch ersetzt EchoSOS nicht den Notruf (Schweiz: 144) und sollte als ergänzende
+				Sicherheitsmaßnahme betrachtet werden.
 			</Text>
 			<Heading as="h3" size="md" mt="8" mb="4">
 				Verhaltenskodex nach einem Unfall
 			</Heading>
-			<Text>
-				Ein Verhaltenskodex nach einem Unfall beschreibt die Regeln und Verhaltensweisen, die nach
-				einem Unfall zu beachten sind. Es gibt einige allgemeine Empfehlungen, die in einem
-				Verhaltenskodex nach einem Unfall enthalten sein sollten:
-			</Text>
-			<OrderedList my="4" px="6">
+			<Text>Nach einem Unfall sollten folgende Verhaltensregeln beachtet werden:</Text>
+			<OrderedList fontWeight="semibold" my="4" px="6">
 				<ListItem>
-					Sicherheit geht vor: Stellen Sie sicher, dass alle Beteiligten in Sicherheit sind und,
-					falls erforderlich, medizinische Hilfe in Anspruch nehmen.
+					Sicherheit gewährleisten:
+					<Text as="span" fontWeight="normal">
+						Stellen Sie sicher, dass alle Beteiligten in Sicherheit sind und holen Sie
+						gegebenenfalls medizinische Hilfe.
+					</Text>
 				</ListItem>
 				<ListItem>
-					Unfall melden: Melden Sie den Unfall der Polizei und/oder dem Unfallversicherer, falls
-					erforderlich.
+					Unfall melden:
+					<Text as="span" fontWeight="normal">
+						Informieren Sie die Polizei und/oder den Unfallversicherer.
+					</Text>
 				</ListItem>
 				<ListItem>
-					Beweise sichern: Machen Sie Fotos von der Unfallstelle und beschädigen Sie keine
-					Beweismittel.
+					Beweissicherung:
+					<Text as="span" fontWeight="normal">
+						Machen Sie Fotos von der Unfallstelle und sichern Sie relevante Beweise.
+					</Text>
 				</ListItem>
 				<ListItem>
-					Keine Schuldzuweisungen: Vermeiden Sie es, Schuldzuweisungen zu machen oder Vorwürfe zu
-					erheben.
+					Keine Schuldzuweisungen:
+					<Text as="span" fontWeight="normal">
+						Vermeiden Sie Vorwürfe und Schuldzuweisungen.
+					</Text>
 				</ListItem>
 				<ListItem>
-					Kooperation: Kooperieren Sie mit den Ermittlungsbehörden und den Versicherungen, um den
-					Unfall schnellstmöglich aufzuklären.
+					Kooperation:
+					<Text as="span" fontWeight="normal">
+						Arbeiten Sie mit den Ermittlungsbehörden und Versicherungen zusammen, um den Unfall
+						schnell aufzuklären.
+					</Text>
 				</ListItem>
 			</OrderedList>
 			<Text fontWeight="semibold">
-				Es ist wichtig, dass alle Beteiligten nach einem Unfall verantwortungsbewusst handeln und
-				sich an diese Regeln halten, um unnötige Konflikte und Probleme zu vermeiden.
+				Diese Regeln helfen, den Unfall sachgerecht zu klären und Konflikte zu vermeiden.
 			</Text>
 		</>
 	);
