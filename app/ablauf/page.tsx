cf7488c39c17078f65c7ad654541779a42218e27
FrediLehmann <frederic.lehmann@bluewin.ch>
2024-11-30 14:45:38 +0100

    add timeline page

---
+++ app/ablauf/page.tsx
@@ -0,0 +1,92 @@
+import { Heading, ListItem, OrderedList, Text } from '@chakra-ui/react';
+
+export const metadata = {
+	title: 'Velogruppe 60+ Sensetal | Ablauf',
+	description: 'Ablauf einer Fahrradtour der Velogruppe 60+ Sensetal'
+};
+
+export default function Unfallverhlaten() {
+	return (
+		<>
+			<Heading as="h2" size="lg" mt="8" mb="4">
+				Ablauf der eBike-Tour mit Tourenleitung, Gruppenleitung, Schlussmann und Patrouillendienst
+			</Heading>
+			<OrderedList fontWeight="semibold" my="4" px="6">
+				<ListItem>
+					Begrüssung und Einweisung
+					<Text as="span" fontWeight="normal">
+						Die eBike-Tour beginnt mit einer freundlichen Begrüssung durch den Tourenleiter, der für
+						die -Gesamtkoordination und einen reibungslosen Ablauf verantwortlich ist. Nach der
+						Vorstellung des Tourverlaufs und einer Sicherheitsunterweisung werden die Teilnehmenden
+						in Kleingruppen eingeteilt. Jede Gruppe steht unter der Leitung eines Gruppenleiters,
+						der für das Wohl und die Sicherheit der Gruppe verantwortlich zeichnet.
+					</Text>
+				</ListItem>
+				<ListItem>
+					Aufwärmphase und Gruppenbildung (ca. 10 Minuten)
+					<Text as="span" fontWeight="normal">
+						Eine gemeinsame Aufwärmphase, durchgeführt von einer fachkundigen Person, dient zur
+						Vorbereitung der Muskulatur auf die bevorstehende Aktivität und minimiert
+						Verletzungsrisiken. Anschliessend erfolgt die endgültige Gruppenbildung in Einheiten von
+						6 bis 8 Personen. Jede Gruppe wird von einem Schlussmann begleitet, der durch eine
+						auffällige Warnweste identifizierbar ist und für die Sicherung des hinteren Bereichs der
+						Gruppe verantwortlich ist.
+					</Text>
+				</ListItem>
+				<ListItem>
+					Start der Tour und Hauptstrecke
+					<Text as="span" fontWeight="normal">
+						<Text>
+							Nach einer kurzen Einweisung gibt der Tourenleiter das Startsignal, und die
+							Kleingruppen setzen sich nacheinander in Bewegung. Beim Überqueren befahrener Strassen
+							koordiniert der Patrouillendienst den Verkehr, um allen Teilnehmenden eine sichere
+							Passage zu ermöglichen. Der Schlussmann überwacht dabei den Abschluss und stellt
+							sicher, dass niemand den Anschluss verliert.
+						</Text>
+						<Text mt="3">
+							Die anschliessende Tour führt über eine landschaftlich reizvolle Route, die
+							abwechslungsreiche Eindrücke bietet. Der Tourenleiter stimmt das Tempo auf die
+							individuellen Fähigkeiten der Teilnehmenden ab. Währenddessen fördern die
+							Gruppenleiter die Motivation und das harmonische Miteinander in ihren Kleingruppen.
+							Der Schlussmann sorgt dafür, dass die Gruppe geschlossen bleibt und alle sicher die
+							Strecke bewältigen.
+						</Text>
+					</Text>
+				</ListItem>
+				<ListItem>
+					Rast und Verpflegung
+					<Text as="span" fontWeight="normal">
+						Nach der intensiven Etappe steht eine Pause in einem gemütlichen Restaurant auf dem
+						Programm. Die Teilnehmenden können sich bei Getränken und Snacks stärken und die
+						entspannte Atmosphäre geniessen. Diese Rast bietet auch eine ideale Gelegenheit, sich
+						auszutauschen und neue Kontakte zu knüpfen. Der Tourenleiter achtet darauf, dass die
+						Pause ausreichend lang ist, um allen eine optimale Regeneration zu ermöglichen.
+					</Text>
+				</ListItem>
+				<ListItem>
+					Rückfahrt und Ausklang
+					<Text as="span" fontWeight="normal">
+						Die Rückfahrt erfolgt in einem gemässigten Tempo, damit alle die Umgebung geniessen
+						können. Gruppenleiter und Schlussmänner gewährleisten weiterhin Sicherheit und ein
+						harmonisches Miteinander in den Gruppen.
+					</Text>
+				</ListItem>
+				<ListItem>
+					Dehnübungen und Abschluss
+					<Text as="span" fontWeight="normal">
+						Zur Förderung der Regeneration wird eine kurze Einheit mit Dehnübungen angeboten. Diese
+						wird von einer geschulten Person geleitet und trägt zur Entspannung der Muskulatur bei.
+					</Text>
+				</ListItem>
+				<ListItem>
+					Verabschiedung und Ausblick auf die nächste Tour
+					<Text as="span" fontWeight="normal">
+						Zum Abschluss dankt der Tourenleiter den Teilnehmenden für ihr Engagement und gibt einen
+						Ausblick auf die nächste Tour, die auf der Website der Velogruppe angekündigt wird. Ein
+						motivierender Schlusspunkt, der die Vorfreude auf weitere gemeinsame Erlebnisse weckt.
+					</Text>
+				</ListItem>
+			</OrderedList>
+		</>
+	);
+}
--- app/info/components/AdditionalInfos/AdditionalInfos.tsx
+++ app/info/components/AdditionalInfos/AdditionalInfos.tsx
@@ -16,6 +16,11 @@ export default function AdditionalInfos() {
 				Wo finde ich weitere Informationen?
 			</Heading>
 			<Flex flexDirection={['column', 'row']} gap={['2', '6']}>
+				<TrackClickEvent event={{ name: 'TIMELINE_PAGE_LINK_CLICK' }}>
+					<Button href="/ablauf" as={Link} rightIcon={<ArrowRight boxSize="5" />}>
+						Ablauf
+					</Button>
+				</TrackClickEvent>
 				<TrackClickEvent event={{ name: 'ACCIDENT_PAGE_LINK_CLICK' }}>
 					<Button href="/unfallverhalten" as={Link} rightIcon={<ArrowRight boxSize="5" />}>
 						Unfallverhalten
