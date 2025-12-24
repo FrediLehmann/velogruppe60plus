c30cf5ae5b7e57a4f28959d1d33fd5204bb1a9cb
FrediLehmann <frederic.lehmann@bluewin.ch>
2025-12-24 15:00:42 +0100

    Add required star to descent

--- app/admin/components/TourForm/TourForm.tsx
+++ app/admin/components/TourForm/TourForm.tsx
@@ -201,7 +201,10 @@ export default function TourForm({
 									<Field.Root
 										required
 										invalid={(form.errors.descent && form.touched.descent) as boolean}>
-										<Field.Label>Abstieg</Field.Label>
+										<Field.Label>
+											Abstieg
+											<Field.RequiredIndicator />
+										</Field.Label>
 										<Input {...field} />
 										<Field.ErrorText>{form.errors?.descent as string}</Field.ErrorText>
 									</Field.Root>
